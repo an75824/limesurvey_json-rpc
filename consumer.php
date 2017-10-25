3464ef3641e929fbcda76f70bfbb342a574effde
antony13 <nanosa@uni.coventry.ac.uk>
2017-10-25 15:36:38 +0100

    Simple code that communications with a Limesurvey server

---
+++ consumer.php
@@ -0,0 +1,24 @@
+<?php
+require_once('jsonRPCClient.php'); //Include the RPC class
+require_once('credentials.php'); //this file contains your username and password
+
+$url = 'http://127.0.0.1/limesurvey/index.php/admin/remotecontrol';
+
+$client = new JsonRPCClient($url);//instance of a new client
+
+// create a session key
+$session_key= $client->get_session_key($username,$password); //if success: str. If not success: array
+
+if (!is_string($session_key))
+{
+	exit("Invalid username or password\n");
+}
+//List the available surveys
+$surveys = $client->list_surveys($session_key,'admin'); //array 2x Dimensional
+foreach ($surveys as $index=>$survey)
+{
+	echo $survey['surveyls_title']."\n";
+}
+
+// remove the session key
+$client->release_session_key($session_key);
